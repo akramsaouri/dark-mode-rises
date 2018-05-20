b0ead75e863b9a9eb32b7ddd002f2fd8d0b4b56d
akramsaouri <akram@hiddenfounders.com>
2018-05-20 22:35:08 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,129 @@
+const websites = [{
+  "name": "Twitter",
+  "hostname": "twitter.com",
+  "selectors": [{
+    "query": "#user-dropdown-toggle",
+    "action": "click"
+  }, {
+    "query": ".nightmode-toggle a"
+  }]
+}, {
+  "name": "Youtube",
+  "hostname": "youtube.com",
+  "selectors": [{
+    "query": "button.ytd-topbar-menu-button-renderer",
+    "action": "click"
+  }, {
+    "query": ".ytd-popup-container .ytd-multi-page-menu-renderer .ytd-multi-page-menu-renderer:nth-child(2) .yt-multi-page-menu-section-renderer .yt-multi-page-menu-section-renderer",
+    "action": "click"
+  },
+  {
+    "query": ".ytd-popup-container #submenu #caption-container .ytd-toggle-item-renderer:last-child",
+  }]
+}, {
+  "name": "Wikiwand",
+  "hostname": "wikiwand.com",
+  "selectors": [{
+    "query": ".per_btn",
+    "action": "click"
+  }, {
+    "query": ".theme",
+  }]
+}]
+
+function getSunriseSunset() {
+  const coordinates = {
+    lat: 33.589886,
+    lng: -7.603869
+  }
+  const url = `https://api.sunrise-sunset.org/json?lat=${coordinates.lat}&lng=${coordinates.lng}`
+  // mocking sunrise sunset api for now
+  return Promise.resolve({
+    "sunrise": "5:26:43 AM",
+    "sunset": "7:27:07 PM",
+  })
+}
+
+function isAfter(x, y) {
+  return x.h > y.h || (x.h === y.h && x.m > y.m)
+}
+
+function strToDate(str, isPm) {
+  const arr = str.split(':')
+  const h = parseInt(arr[0])
+  const m = parseInt(arr[1])
+  return {
+    h: isPm ? h + 12 : h,
+    m
+  }
+}
+
+function walkDom(selectors, cb) {
+  for (let index = 0; index < selectors.length; index++) {
+    const selector = selectors[index];
+    setTimeout(() => {
+      const element = document.querySelector(selector.query)
+      if (selector.action) {
+        element[selector.action]()
+      }
+      if (index === selectors.length - 1) {
+        cb(element)
+      }
+    }, (index + 1) * 1000);
+  }
+}
+
+function handleTwitter(isDayTime, element) {
+  const s = Array.from(element.querySelector('span').classList)
+  const isNightModeActive = s.includes('Icon--crescentFilled')
+  if (isDayTime && isNightModeActive) {
+    localStorage.setItem('lastTheme', 'day')
+    element.click()
+  }
+  if (!isDayTime && !isNightModeActive) {
+    localStorage.setItem('lastTheme', 'night')
+    element.click()
+  }
+}
+
+function handleYoutube(isDayTime, element) {
+  const isNightModeActive = element.getAttribute('aria-pressed') === 'true'
+  if (isDayTime && isNightModeActive) {
+    localStorage.setItem('lastTheme', 'day')
+    element.click()
+  }
+  if (!isDayTime && !isNightModeActive) {
+    localStorage.setItem('lastTheme', 'night')
+    element.click()
+  }
+}
+
+function handleWikiwand(isDayTime, element) {
+  const isNightModeActive = Array
+    .from(element.querySelector('.noUi-origin').classList)
+    .includes('noUi-stacking')
+  if (isDayTime && isNightModeActive) {
+    localStorage.setItem('lastTheme', 'day')
+    element.querySelector('span:first-child').click()
+  }
+  if (!isDayTime && !isNightModeActive) {
+    localStorage.setItem('lastTheme', 'night')
+    element.querySelector('span:last-child').click()
+  }
+}
+
+(async () => {
+  const { sunrise, sunset } = await getSunriseSunset()
+  const now = { h: new Date().getHours(), m: new Date().getMinutes() }
+  // const now = { h: strToDate(sunrise).h + 1, m: 0 }
+  const isDayTime = isAfter(now, strToDate(sunrise)) && isAfter(strToDate(sunset, true), now)
+  const lastTheme = localStorage.getItem('lastTheme')
+  if ((isDayTime && lastTheme === 'day')
+    || (!isDayTime && lastTheme === 'night')) return
+  // console.log('isDayTime:', isDayTime)
+  const hostname = location.hostname.replace('www.', '')
+  const website = websites.find(w => w.hostname === hostname)
+  walkDom(website.selectors, element => {
+    eval(`handle${website.name}(isDayTime, element)`)
+  })
+})()
---
+++ readme.md
@@ -0,0 +1,17 @@
+# dark-mode-rises
+
+Lightweight chrome extension for automatically enabling/disabling dark mode on twitter, youtube and wikiwand.
+
+### Installation
+
+- Download this repository as [ZIP](https://github.com/akramsaouri/dark-mode-rises/archive/master.zip) archive and Unpack it
+- Go to Chrome -> Extensions (enter chrome://extensions/ into the URL bar)
+- Enable the `developer mode` if not enabled
+- Click `Load unpacked extensions` and select the unpacked ZIP archive
+
+Done! 🎊
+
+### Improvements
+- custom sunset/sunrise location (works for morocco only for now)
+- progammatically set querySelector timeout
+- support both manual and automatic night mode enabling
